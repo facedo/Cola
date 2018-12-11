1b023af10df44fc9146ed6979c955a27e4e5f37c
Facedo <facedo2012@gmail.com>
2018-12-10 22:41:04 -0300

    atender

--- app/controllers/atender_controller.rb
+++ app/controllers/atender_controller.rb
@@ -2,5 +2,7 @@ class AtenderController < ApplicationController
   
   def index
   	@fila = Fila.find(1)
+  	@fila_puestos = @fila.fila_puestos.where(fecha: Date.today)
+  	$nom_mod = "Atender"
   end
 end
